d6ee279aa3f7e35495af0dcad75a5982a01f5aa2
Kavin Kumar <kavinkumarnkm007@gmail.com>
2024-08-21 00:18:17 +0530

    feat:  update logo

--- src/components/HeaderCard.jsx
+++ src/components/HeaderCard.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import logo from '../assets/logo1.png'
+
 
 const HeaderCard = () => {
   return (
     <header className='flex p-5 justify-between'>
-      <h1 className='title text-3xl font-bold'>Stock Grid</h1>
+      <h1 className='title text-3xl font-bold flex gap-2'>
+        <img src={logo} alt="Logo" className='h-8 w-8'/>
+        Stock Grid
+      </h1>
       <h2 className='text-xl mt-1'>Made with ❤️ by Kavin Kumar</h2>
     </header>
   )
